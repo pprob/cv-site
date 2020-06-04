440feb99d226e48921f369ee16bb773a2331c744
unknown <p.probert340@gmail.com>
2020-06-04 16:54:31 +0100

    initial commit

---
+++ src/components/header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = props => {
+  return (
+    <header className='header__container'>
+      <nav className='header__container__nav'></nav>
+      <div className='header__container__socials'></div>
+    </header>
+  )
+}
+
+export default Header
